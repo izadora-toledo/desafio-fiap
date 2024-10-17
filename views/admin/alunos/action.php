bb1fac594fdc7a53336492196bfafc622c32b555
Izadora Toledo <izadora.devdescontraida@gmail.com>
2024-10-17 11:05:59 -0300

    ajuste no include, variaveis e mensagem de erro da action de cadastrar alunos

--- views/admin/alunos/action.php
+++ views/admin/alunos/action.php
@@ -5,15 +5,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $nome = $_POST['nome'];
     $data_nascimento = $_POST['data_nascimento'];
-    $usuario = $_POST['usuario'];
+    $usuario_cpf = $_POST['usuario_cpf'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
     
     $controller = new AlunoController();
-    $resultado = $controller->cadastrarAluno($nome, $data_nascimento, $usuario, $email, $telefone);    
+    $resultado = $controller->cadastrarAluno($nome, $data_nascimento, $usuario_cpf, $email, $telefone);    
 
     echo $resultado;
 } else {
-    echo "Erro ao cadastrar aluno";
+    echo '<div style="color:red;">Erro</div>';
 }
 ?>
