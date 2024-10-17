e11a39747a017e79d7ddc709ebd019a203f39df7
Izadora Toledo <izadora.devdescontraida@gmail.com>
2024-10-17 14:16:26 -0300

    Cria formulário de edição e modal pra conter o formulario

---
+++ views/admin/alunos/editar.php
@@ -0,0 +1,63 @@
+<?php 
+include "../../../controllers/AlunoController.php"; 
+
+
+// if (!isset($_SESSION['id_usuario'])) {     
+//     header('Location: ../login/index.php');
+//     exit;
+// }
+
+$id_aluno = $_GET['id'] ?? null; 
+$aluno = []; 
+
+if ($id_aluno) {
+    $alunoController = new AlunoController();
+    $aluno = $alunoController->buscaAlunoPorId($id_aluno);   
+} else {
+    echo '<div class="alert alert-danger">Aluno não encontrado.</div>';
+}
+
+?>
+
+<form id="form-editar-aluno">
+    <input type="hidden" name="id" id="id" value="<?= htmlspecialchars($aluno['id'] ?? '') ?>">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" required value="<?= htmlspecialchars($aluno['nome'] ?? '') ?>">
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="form-group">
+                <label for="data_nascimento">Data de Nascimento:</label>
+                <input type="date" class="form-control" id="data_nascimento" name="data_nascimento" required value="<?= htmlspecialchars($aluno['data_nascimento'] ?? '') ?>">
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-3">
+            <div class="form-group">
+                <label for="usuario_cpf">Usuário (CPF):</label>
+                <input type="text" class="form-control mt-2" id="usuario_cpf" name="usuario_cpf" placeholder="CPF do aluno" maxlength="14" required value="<?= htmlspecialchars($aluno['usuario_cpf'] ?? '') ?>">
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control mt-2" id="email" name="email" placeholder="E-mail" required value="<?= htmlspecialchars($aluno['email'] ?? '') ?>">
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="form-group">
+                <label for="telefone">Telefone:</label>
+                <input type="tel" class="form-control mt-2" id="telefone" name="telefone" required value="<?= htmlspecialchars($aluno['telefone'] ?? '') ?>">
+            </div>
+        </div>
+        <div class="btn-atualizar">
+            <button type="submit" class="btn btn-editar-aluno mt-4">Salvar</button>
+        </div>
+    </div>
+    
+</form>
+<div id="mensagem-retorno-editar" class="mt-3"></div>
--- views/admin/alunos/index.php
+++ views/admin/alunos/index.php
@@ -11,38 +11,65 @@ if (!isset($_SESSION['id_usuario'])) {
 require_once __DIR__ . '/../../../controllers/AlunoController.php';
 
 $alunoController = new AlunoController();
-$alunos = $alunoController->listarAlunos();
+$alunos = $alunoController->listaAlunos();
 ?>
 
 <div class="listar-alunos mt-5">
     <h2>Lista de Alunos</h2>
 
-    <?php if (is_array($alunos) && count($alunos) > 0): ?>
+    <?php if (is_array($alunos) && count($alunos) > 0) { ?>
         <table class="table table-bordered">
             <thead>
                 <tr>
+                    <th>Id</th>
                     <th>Nome</th>
                     <th>Data de Nascimento</th>
                     <th>Usuário</th>
                     <th>Email</th>
                     <th>Telefone</th>
+                    <th>Ações</th> <!-- Nova coluna para ações -->
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($alunos as $aluno): ?>
+                <?php foreach ($alunos as $aluno) { ?>
                     <tr>
+                        <td><?= $aluno['id']; ?></td>
                         <td><?= htmlspecialchars(strtoupper($aluno['nome'])) ?></td>
                         <td><?= htmlspecialchars(Funcoes::converterDataParaBR($aluno['data_nascimento'])) ?></td>
                         <td><?= htmlspecialchars($aluno['usuario_cpf']) ?></td>
                         <td><?= htmlspecialchars($aluno['email']) ?></td>
                         <td><?= htmlspecialchars($aluno['telefone']) ?></td>
+                        <td>                      
+                        <td>
+                        <button class="btn btn-sm btn-editar" data-id="<?= $aluno['id']; ?>">Editar</button>                 
+                        </td>
+                    
+                       
                     </tr>
-                <?php endforeach; ?>
+                <?php } ?>
             </tbody>
         </table>
-    <?php else: ?>
+    <?php } else { ?>
         <p>Nenhum aluno encontrado.</p>
-    <?php endif; ?>
+    <?php } ?>
+</div>
+<div id="modal-editar-aluno" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog custom-width" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Editar Aluno</h5>            
+                <button type="button" class="close btn-fechar" data-dismiss="modal" aria-label="Fechar">
+                    <span aria-hidden="true">&times;</span>
+                </button>            
+            </div>
+            <div class="modal-body">
+                <div id="conteudo-modal"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-fechar" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php require '../../../public/includes/footer.php'; ?>
